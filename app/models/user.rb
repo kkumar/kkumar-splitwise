f062ac942b79e6861999055b910293a7ba312328
Kiran <jha.kkumar@gmail.com>
2024-04-21 12:56:24 +0530

    Associations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :expenses, foreign_key: 'paid_by_id'
+  has_many :user_connections
+  has_many :connections, through: :user_connections
+  has_many :expense_item_participants
+  has_many :expense_items, through: :expense_item_participants
+  has_many :sent_settlements, class_name: 'Settlement', foreign_key: 'sender_id'
+  has_many :received_settlements, class_name: 'Settlement', foreign_key: 'receiver_id'    
 end
